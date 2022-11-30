6e7de8804a99e324c5f86b166fd2b57386aafae1
RioFrancisAikman <115077816+RioFrancisAikman@users.noreply.github.com>
2022-11-30 13:01:03 +0000

    Fixed Vector3 script

--- Assets/Scripts/DistanceScript.cs
+++ Assets/Scripts/DistanceScript.cs
@@ -16,30 +16,6 @@ public class DistanceScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        //get the distance to player using Vector3.Distance
-        float distanceToPlayer = Vector3.Distance(capsule.transform.position, transform.position);
-
-        Debug.Log("player is" + distanceToPlayer + "away from us.");
-
-
-        //
-
-        float x = capsule.transform.position.x - transform.position.x;
-        float y = capsule.transform.position.y - transform.position.y;
-        float z = capsule.transform.position.z - transform.position.z;
-
-        //
-        x = Mathf.Pow(x, 2);
-        y = Mathf.Pow(y, 2);
-        z = Mathf.Pow(z, 2);
-
-        //
-        float sqXYZ = x + y + z;
-        //
-        float calcDistance = Mathf.Sqrt(sqXYZ);
-        Debug.Log("Our calculated distance is " + calcDistance);
-        */
 
         Debug.Log(CalculateDistanceBetweenVectors(player.transform.position, transform.position));
 
@@ -60,7 +36,7 @@ public class DistanceScript : MonoBehaviour
 
     public float CalculateDistanceBetweenVectors(Vector3 v1, Vector3 v2)
     {
-        //square root if 1.x-2.x squared, plus 1.y-2.y squared
+        //square root of 1.x-2.x squared, plus 1.y-2.y squared
         float x = v1.x - v2.x;
         float y = v1.y - v2.y;
         float z = v1.z - v2.z;
@@ -69,6 +45,14 @@ public class DistanceScript : MonoBehaviour
         x = Mathf.Pow(x, 2);
         y = Mathf.Pow(y, 2);
         z = Mathf.Pow(z, 2);
+
+        // add the squared values
+        float sqXYZ = x + y + z;
+        //
+
+        return Mathf.Sqrt(sqXYZ);
+
+
     }
-    
+
 }
